a827270b7d3b53f561f0cf472b92a74a17874df9
Asuka Wang <hungmao.wang@linecorp.com>
2023-03-27 17:44:47 +0900

    feat: receive url list from request

--- functions/index.js
+++ functions/index.js
@@ -3,12 +3,26 @@ const axios = require("axios");
 const convert = require('xml-js');
 
 exports.getRssFead = functions.https.onRequest(async (request, response) => {
-  const RSS_URL = 'https://hnrss.org/frontpage';
+  if (request.method !== 'POST') {
+    response.status(400).send('Request is not a POST');
+  }
+  const body = request.body;
+  if (!body) {
+    response.status(400).send('Body is empty');
+  }
+  const { rssUrlList } = body;
 
-  axios.get(RSS_URL)
-    .then(res => {
+  const rssFetchPromises = rssUrlList.map(rssUrl => new Promise(resolve => {
+    axios.get(rssUrl).then(res => {
       const jsonRes = convert.xml2json(res.data);
-      response.send(jsonRes);
+      resolve(jsonRes);
     })
-})
+  }))
 
+  Promise
+    .allSettled(rssFetchPromises)
+    .then((results) => {
+      const feeds = results.filter(result => result.status === 'fulfilled').map(result => result.value);
+      response.status(200).send(feeds)
+    });
+});
